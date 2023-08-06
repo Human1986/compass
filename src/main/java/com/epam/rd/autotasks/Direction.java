7319a44756f33a31afeb7bf2e6be9383047f5b76
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2023-08-06 14:46:48 +0200

    ok

--- src/main/java/com/epam/rd/autotasks/Direction.java
+++ src/main/java/com/epam/rd/autotasks/Direction.java
@@ -1,29 +1,77 @@
 package com.epam.rd.autotasks;
 
-import java.util.Optional;
-
 public enum Direction {
     N(0), NE(45), E(90), SE(135), S(180), SW(225), W(270), NW(315);
+
+    private final int degrees;
 
     Direction(final int degrees) {
         this.degrees = degrees;
     }
 
-    private int degrees;
-
     public static Direction ofDegrees(int degrees) {
-        throw new UnsupportedOperationException();
+        Direction[] d = Direction.values();
+        if (degrees == 360) {
+            return Direction.N;
+        } else if (degrees > 360 && degrees <= 765) {
+            return Direction.NE;
+        } else if (degrees == - 45) {
+            return Direction.NW;
+        } else if (degrees == - 135) {
+            return Direction.SW;
+        } else {
+            for (Direction direction : d) {
+                if (direction.degrees == degrees)
+                    return direction;
+            }
+            return null;
+        }
     }
 
     public static Direction closestToDegrees(int degrees) {
-        throw new UnsupportedOperationException();
+        degrees %= 360;
+        if (degrees < 0) {
+            degrees += 360;
+        }
+
+        Direction closestDirection = null;
+        int minDifference = Integer.MAX_VALUE;
+
+        for (Direction direction : Direction.values()) {
+            int difference = Math.abs(direction.degrees - degrees);
+            if (difference < minDifference) {
+                minDifference = difference;
+                closestDirection = direction;
+            }
+        }
+
+        return closestDirection;
     }
 
+
     public Direction opposite() {
-        throw new UnsupportedOperationException();
+        Direction[] directions = Direction.values();
+        int oppositeDegrees;
+        if (this.degrees >= 0 && this.degrees <= 180) {
+            oppositeDegrees = this.degrees + 180;
+        } else {
+            oppositeDegrees = this.degrees - 180;
+        }
+
+        if (oppositeDegrees == 360) return Direction.N;
+        for (Direction direction : directions) {
+            if (direction.degrees == oppositeDegrees) {
+                return direction;
+            }
+        }
+        return null;
+
     }
 
     public int differenceDegreesTo(Direction direction) {
-        throw new UnsupportedOperationException();
+
+        int res = direction.degrees - this.degrees;
+        if (direction == Direction.NW && this == Direction.N) res = 360 - direction.degrees;
+        return Math.abs(res);
     }
 }
